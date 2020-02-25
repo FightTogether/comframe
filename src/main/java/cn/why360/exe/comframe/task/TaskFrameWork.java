867118b2517cef9246a0e1a3abf58fec853c6dc7
FightTogether <24369952@qq.com>
2020-02-26 02:52:22 +0800

    commit

---
+++ src/main/java/cn/why360/exe/comframe/task/TaskFrameWork.java
@@ -0,0 +1,20 @@
+package cn.why360.exe.comframe.task;
+
+/**
+ *   
+ *  * Copyright: Copyright (c) 2020 Asiainfo
+ *  * 
+ *  * @ClassName: cn.why360.exe.comframe.task.TaskFrameWork
+ *  * @Description: 该类的功能描述
+ *  *
+ *  * @version: v1.0.0
+ *  * @author: wenghy   
+ *  * @date: 2020/2/26 2:52 AM 
+ *  *
+ *  * Modification History:
+ *  * Date         Author          Version            Description
+ *  *------------------------------------------------------------
+ *  * 2020/2/26      wenghy          v1.0.0               修改原因
+ */
+public class TaskFrameWork {
+}
